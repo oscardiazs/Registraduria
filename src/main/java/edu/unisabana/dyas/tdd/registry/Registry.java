32faeb21bd68b6871e6b1da1e88bfd20fc3602e3
oscardiazs <diazsanchezoscar@gmail.com>
2024-06-08 23:51:17 -0400

    Completado

--- src/main/java/edu/unisabana/dyas/tdd/registry/Registry.java
+++ src/main/java/edu/unisabana/dyas/tdd/registry/Registry.java
@@ -1,7 +1,33 @@
 package edu.unisabana.dyas.tdd.registry;
+
+import java.util.HashSet;
+import java.util.Set; // Se usa para tener un registro
+
 public class Registry {
+
+    private Set<Integer> registroPvalidado = new HashSet<>();
+
     public RegisterResult registerVoter(Person p) {
-        // TODO Validate person and return real result.
+        
+        // Si se indica que la persona no esta viva
+        if (!p.isAlive()) {
+            return RegisterResult.DEAD;
+        }
+        // La persona no puede ser menor de 18 anos
+        if (p.getAge() > 0 && p.getAge() < 18) {
+            return RegisterResult.UNDERAGE;
+        }
+        // La persona debe tener entre 18 y 120 anos segun nuestro caso
+        if (p.getAge() < 18 || p.getAge() > 120) {
+            return RegisterResult.INVALID_AGE;
+        }
+        // Si ya el registro fue hecho anteriormente
+        if (registroPvalidado.contains(p.getId())) {
+            return RegisterResult.DUPLICATED;
+        }
+        // Si no se ha regresado nigun otro valor, la persona es valida
+        registroPvalidado.add(p.getId());
         return RegisterResult.VALID;
     }
+    
 }
--- src/test/java/edu/unisabana/dyas/tdd/registry/RegistryTest.java
+++ src/test/java/edu/unisabana/dyas/tdd/registry/RegistryTest.java
@@ -5,12 +5,53 @@ import org.junit.Test;
 
 public class RegistryTest {
     private Registry registry = new Registry();
+    
+    // Test 1 validar si puede votar con datos finales
     @Test
     public void validateRegistryResult() {
-        Person person1 = new Person("Oscar",18764,18,Gender.MALE,false);
-        registry.registerVoter(person1);
-        RegisterResult result = registry.registerVoter(person1);
+        Person person = new Person("Oscar",18760,18,Gender.MALE,true);
+        //registry.registerVoter(person);
+        RegisterResult result = registry.registerVoter(person);
         Assert.assertEquals(RegisterResult.VALID, result);
     }
-    // TODO Complete with more test cases
+
+    // Test 2 validar si esta vivo
+    @Test
+    public void validateAlive() {
+        Person person = new Person("Juan",18765,25,Gender.MALE,false);
+        //registry.registerVoter(person);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.DEAD, result);
+    }
+
+    // Test 3 validar la edad del votante
+    @Test
+    public void validateAge() {
+        Person person = new Person("Ana",18324,15,Gender.FEMALE,true);
+        //registry.registerVoter(person);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.UNDERAGE, result);
+    }
+
+    // Test 4 validar la edad mal con un numero negativo
+    @Test
+    public void validateNegativeAge() {
+        Person person = new Person("Sofia",24625,-56,Gender.FEMALE,true);
+        //registry.registerVoter(person);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.INVALID_AGE, result);
+    }
+
+    // Test 5 validar duplicado, se basa uniccamente en el ID
+    @Test
+    public void validateDuplicate() {
+        Person person = new Person("Juan del Sur",99875,43,Gender.MALE,true);
+        Person person1 = new Person("Juana del Sur",99875,54,Gender.FEMALE,true);
+        //registry.registerVoter(person);
+        //registry.registerVoter(person1);
+        RegisterResult result = registry.registerVoter(person);
+        RegisterResult result1 = registry.registerVoter(person1);
+        Assert.assertEquals(RegisterResult.DUPLICATED, result1);
+    }
+    
 }
